2d590ecc3bc29f872e3def4d259bfde46640db1f
zertyg1 <abdoxvito@gmail.com>
2025-01-06 11:34:21 +0100

    L'utilisateur gagne si la somme des dés est égale au nombre choisi.

--- app/src/main/java/com/adansar/myapplication/MainActivity.kt
+++ app/src/main/java/com/adansar/myapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package com.adansar.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -11,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rollButton: Button
     private lateinit var dice2: ImageView
     private lateinit var resultText: TextView
+    private lateinit var targetNumberInput: EditText
 
     private val diceImages = listOf(
         R.drawable.dice_1,
@@ -31,9 +35,22 @@ class MainActivity : AppCompatActivity() {
         dice2 = findViewById(R.id.dice2)
         resultText = findViewById(R.id.resultText)
 
+        rollButton.isEnabled = false
+
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        targetNumberInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val number = s.toString().toIntOrNull()
+                rollButton.isEnabled = number != null && number in 2..12
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun rollDice() {
@@ -43,10 +60,12 @@ class MainActivity : AppCompatActivity() {
         dice1.setImageResource(diceImages[dice1Value - 1])
         dice2.setImageResource(diceImages[dice2Value - 1])
 
+        val sum = dice1Value + dice2Value
+
         if (dice1Value == dice2Value) {
             resultText.text = "Félicitations ! Vous avez gagné !"
         } else {
-            resultText.text = "Essayez encore !"
+            resultText.text = "Dommage ! Essayez encore (Somme: $sum)"
         }
     }
 }
